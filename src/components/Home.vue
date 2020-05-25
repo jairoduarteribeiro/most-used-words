88cf45bfbf2f08fb20149903d2f77f2d8bda9141
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-05-25 14:23:22 -0300

    Create a box to get the files from the system

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
@@ -12,27 +24,18 @@ export default {
   components: { Pill },
   data: () => {
     return {
+      files: [],
       groupedWords: [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
         { name: "i", amount: 1234 },
         { name: "you", amount: 900 },
         { name: "he", amount: 853 }
       ]
     };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    }
   }
 };
 </script>
