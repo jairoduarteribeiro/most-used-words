4ebd142a8f54a4fa2ccc25d8f287bb1de8d61d28
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-05-26 13:54:18 -0300

    Transform file contents to rows in an array

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,17 @@
 const { ipcMain } = require('electron')
+const pathsToRows = require('./pathsToRows')
 
 ipcMain.on('process-subtitles', (event, paths) => {
-  console.log(paths)
-
-  event.reply('process-subtitles', [
-    { name: "i", amount: 1234 },
-    { name: "you", amount: 900 },
-    { name: "he", amount: 853 },
-    { name: "she", amount: 853 },
-    { name: "our", amount: 133 },
-    { name: "house", amount: 23 }
-  ])
+  pathsToRows(paths)
+    .then(rows => console.log(rows))
+    .then(() => {
+      event.reply('process-subtitles', [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "she", amount: 853 },
+        { name: "our", amount: 133 },
+        { name: "house", amount: 23 }
+      ])
+    })
 })
