ef2a7a4db75dc893441541083f56230753bc7b27
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-05-27 15:48:52 -0300

    Finish the app

---
+++ src/backend/groupWords.js
@@ -0,0 +1,18 @@
+module.exports = words => {
+  return new Promise((resolve, reject) => {
+    try {
+      const groupedWords = words.reduce((obj, word) => {
+        obj[word] = obj[word] ? obj[word] + 1 : 1
+        return obj
+      }, {})
+
+      const groupedWordsArray = Object.keys(groupedWords)
+        .map(key => ({ name: key, amount: groupedWords[key] }))
+        .sort((w1, w2) => w2.amount - w1.amount)
+
+      resolve(groupedWordsArray)
+    } catch (exception) {
+      reject(exception)
+    }
+  })
+}
